5422af5defbe6fc0e0637f9a57f63d605806a424
satson09 <satsunjohnson@gmail.com>
2024-10-06 18:21:14 +0000

    Unittests in JS

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,26 @@
-const calculateNumber = require("./0-calcul.js");
+// 0-calcul.test.js
 const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-    it('rounding of a', () => {
-        assert.equal(calculateNumber(15.78, 2), 18);
-        assert.equal(calculateNumber(1.2, 0), 1);
-        assert.equal(calculateNumber(3.5, 2), 6);
-    });
+describe('calculateNumber', function() {
+  it('should return 4 when 1 and 3 are passed', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
 
-    it('rounding of b', () => {
-        assert.equal(calculateNumber(2, 15.78), 18);
-        assert.equal(calculateNumber(0, 1.2), 1);
-        assert.equal(calculateNumber(2, 3.5), 6);
-    });
+  it('should return 5 when 1 and 3.7 are passed', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
 
-    it('suming of a and b', () => {
-        assert.equal(calculateNumber(15.78, 2.5), 19);
-        assert.equal(calculateNumber(1.2, 0.2), 1);
-        assert.equal(calculateNumber(3.5, 2.7), 7);
-    });
-})
+  it('should return 5 when 1.2 and 3.7 are passed', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when 1.5 and 3.7 are passed', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 when 0 and 0 are passed', function() {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+});
+
