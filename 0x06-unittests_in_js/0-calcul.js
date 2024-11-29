2d5ad65553fcfde8b255fe237545f1cedfa180b1
satson09 <satsunjohnson@gmail.com>
2024-11-29 22:13:40 +0100

    0x06. Unittests in JS

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,12 +1,8 @@
-
 function calculateNumber(a, b) {
     const an = Math.round(a);
     const bn = Math.round(b);
     const c = an + bn;
     return c;
-// 0-calcul.js
-function calculateNumber(a, b){
-    return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,62 +1,31 @@
-
 const assert = require("assert");
 const { it, describe } = require("mocha");
 const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', function () {
+describe("calculateNumber()", function() {
 
-  it('should return 4 when inputs are 1 and 3', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when inputs are 1 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when inputs are 1.2 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when inputs are 1.5 and 3.7', function () {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should handle negative numbers correctly', function () {
-    assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
-  });
-
-  it('should return the rounded sum of two large numbers', function () {
-    assert.strictEqual(calculateNumber(1000.1, 2000.9), 3001);
-  });
-
-  it('should handle one number being 0', function () {
-    assert.strictEqual(calculateNumber(0, 4.5), 5);
-  });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
+    });
 });
-
-// 0-calcul.test.js
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
-
-describe('calculateNumber', function() {
-  it('should return 4 when 1 and 3 are passed', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when 1 and 3.7 are passed', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when 1.2 and 3.7 are passed', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when 1.5 and 3.7 are passed', function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('should return 0 when 0 and 0 are passed', function() {
-    assert.strictEqual(calculateNumber(0, 0), 0);
-  });
-});
-
